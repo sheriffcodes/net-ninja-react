021aff204d03ca98f1554e375a2303f43fb6f956
sheriffcodes <sheriffcodes01@gmail.com>
2023-05-02 23:57:25 +0100

    props

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,14 @@
 import { useState } from "react";
+import Bloglist from "./Bloglist";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
         { title: "Welcome party", body: "lorem ipsum...", author: "Yoshi", id: 2 },
         { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 },
-    ])
+    ]) 
     return ( 
-        <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                     <h2>{ blog.title }</h2>
-                     <p> Written by { blog.author }</p>
-                </div>
-            ))}
-        </div>
+    <Bloglist blogs={blogs} title="All blogs!"  />
      );
 }
  
