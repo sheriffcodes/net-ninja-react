f730300000ba87833db8282c8935c00638aa4c98
sheriffcodes <sheriffcodes01@gmail.com>
2023-05-02 19:48:30 +0100

    outputting lists and learning the key property

--- src/Home.js
+++ src/Home.js
@@ -1,24 +1,19 @@
 import { useState } from "react";
 
 const Home = () => {
-    const [name, setName] = useState("mario");
-    const [age, setAge] = useState(25);
-    const Benita = {
-        name: "Beni",
-        smile: "Biggggggg"
-    }
-
-    const handleClick = () => {
-        setName(Benita.name);
-        setAge(30)
-        console.log(name);
-    };
-
+    const [blogs, setBlogs] = useState([
+        { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+        { title: "Welcome party", body: "lorem ipsum...", author: "Yoshi", id: 2 },
+        { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 },
+    ])
     return ( 
         <div className="home">
-            <h2>Homepage</h2>
-            <p>{ name } is { age } years old and smile { Benita.smile }</p>
-            <button onClick={handleClick}>Click me</button>
+            {blogs.map((blog) => (
+                <div className="blog-preview" key={blog.id}>
+                     <h2>{ blog.title }</h2>
+                     <p> Written by { blog.author }</p>
+                </div>
+            ))}
         </div>
      );
 }
