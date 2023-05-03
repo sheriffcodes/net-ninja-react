75d7430192098140944382e0f012ec289cd88477
sheriffcodes <sheriffcodes01@gmail.com>
2023-05-03 13:56:59 +0100

    resuing components with props

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,16 @@ import Bloglist from "./Bloglist";
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-        { title: "Welcome party", body: "lorem ipsum...", author: "Yoshi", id: 2 },
+        { title: "Welcome party", body: "lorem ipsum...", author: "yoshi", id: 2 },
         { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 },
     ]) 
     return ( 
-    <Bloglist blogs={blogs} title="All blogs!"  />
+        <div className="home">
+            <Bloglist blogs={blogs} title="All blogs!"  />
+            <Bloglist blogs={blogs.filter((blog) => 
+            blog.author === "yoshi")} title="Yoshi's Blog"/>
+        </div>
+
      );
 }
  
