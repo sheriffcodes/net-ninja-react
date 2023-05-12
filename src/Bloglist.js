a97a2aebc2966e358f1417e70b01a8b69b8b1591
sheriffcodes <sheriffcodes01@gmail.com>
2023-05-12 02:57:43 +0100

    functions as props (deleting an item)

--- src/Bloglist.js
+++ src/Bloglist.js
@@ -1,4 +1,4 @@
-const Bloglist = ({ blogs, title}) => {
+const Bloglist = ({ blogs, title, handleDelete}) => {
     // const blogs = props.blogs;
     // const title = props.title
 
@@ -9,7 +9,8 @@ const Bloglist = ({ blogs, title}) => {
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                      <h2>{ blog.title }</h2>
-                     <p> Written by { blog.author }</p>
+                     <p> Written by { blog.author }</p> 
+                     <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
                 </div>
             ))}
         </div>
--- src/Home.js
+++ src/Home.js
@@ -7,9 +7,15 @@ const Home = () => {
         { title: "Welcome party", body: "lorem ipsum...", author: "yoshi", id: 2 },
         { title: "Web dev top tips", body: "lorem ipsum...", author: "mario", id: 3 },
     ]) 
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+     
     return ( 
         <div className="home">
-            <Bloglist blogs={blogs} title="All blogs!"  />
+            <Bloglist blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
             <Bloglist blogs={blogs.filter((blog) => 
             blog.author === "yoshi")} title="Yoshi's Blog"/>
         </div>
